2d1e3972a0ee404a5ccd0e8ad1766473c2d531b4
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-01-19 18:25:48 +0530

    adding first changes

---
+++ Python-DSA/python_DataStructures.py
@@ -0,0 +1,11 @@
+# Data Structures are a way of organizing so that is can be accessed more efficiently depending upon the situation.
+#  Data Structures are fundamentals of any programming language around which a program is built. Python helps o learn the fundamental of these data structures in a simpler way as compared to other programming languages.
+
+# Python Data Types
+    #Built in Data Types
+        #0. Strings- orderd,Immutable,arrays of bytes representing Unicode character
+        #1. List - ordered,mutable,allow duplicates
+        #2. Tupple-ordered,immutable,
+        #3. Dictionary- ordered,mutable,allow duplicates
+        #4. Set- ordered,mutable,doesn't allow duplicates
+        #5. Frozen Set- Immutable
